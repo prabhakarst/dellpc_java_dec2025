fd9ca4ba299f1c47f7a5b237c0117b3e34cbf193
Prabhakar.Shenoy <prabhakarshenoyt@gmail.com>
2025-12-26 01:25:16 +0530

    edit loop statements

--- src/test/java/packageOne/O3LoopStatements.java
+++ src/test/java/packageOne/O3LoopStatements.java
@@ -7,6 +7,30 @@ public class O3LoopStatements {
             System.out.println(b);
         b += 1;
     }
+//String hello="     random";
+//        System.out.println(hello.stripLeading());
 
+        //eg:1
+ int do_while=23-3;
+do{
+    System.out.println("print atleast once "+do_while);
+}
+while (do_while%2!=0);
+
+//eg:2
+        int a=12-532;
+        do{
+            System.out.println("goes to infinite if I dont give break because the condition keeps staisfying");
+        break;
+        }
+        while(a%2==0);
+
+
+    //eg:3
+    int cds=900;
+    do{
+        System.out.println(cds);
     }
+while (cds<400);
+}
 }
